230d176bd9030da54ede2a13d2d054f60028f554
marietta-a <msquad095@gmail.com>
2023-10-01 13:51:24 +0100

    deleted the azure pipeline.yml

--- src/app/core/auth-config.ts
+++ src/app/core/auth-config.ts
@@ -2,7 +2,7 @@ import { UserManager, UserManagerSettings } from "oidc-client";
 
 export const config: UserManagerSettings = {
     authority: "https://localhost:5001",
-    client_id: "spa",
+    client_id: "interactive",
     // redirect_uri: "https://localhost:5003/callback.html",
     redirect_uri: "http://localhost:4200/#/dashboard",
     response_type: "code",
